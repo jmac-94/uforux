2c861df57a5529e755db86373bbe8e46975ff33f
nick <nick.sandoval@utec.edu.pe>
2023-12-02 03:13:43 -0500

    calendario 1.0

--- lib/screens/home/calendar.dart
+++ lib/screens/home/calendar.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import 'package:uforuxpi3/models/app_user.dart';
 
+import 'event.dart';
+/*
 class Calendar extends StatefulWidget {
   final AppUser? user;
 
@@ -162,5 +166,141 @@ class _CalendarState extends State<Calendar> {
         ),
       ),
     );
+  }
+}
+*/
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime today = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
+  DateTime? _selectedDay;
+  //Crear eventos
+  Map<DateTime, List<Event>> events = {};
+  //Controlador de texto de eventos
+  TextEditingController _eventController = TextEditingController();
+  //Visualizar si se guardo el evento
+  late ValueNotifier<List<Event>> _selectedEvents;
+
+  //funcion para seleccion dia
+
+  @override
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay))
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _selectedEvents.value = _getEventsForDay(selectedDay);
+      });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    //devuelve el evento guardado
+    return events[day] ?? [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Calendario")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Show el dialogo para el usuario ponga el evento
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  scrollable: true,
+                  title: Text("Event Name"),
+                  content: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: TextField(
+                      controller: _eventController,
+                    ),
+                  ),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        //Guardar el nombre del ebento en el mapa
+                        events.addAll({
+                          _selectedDay!: [Event(_eventController.text)]
+                        });
+
+                        Navigator.of(context).pop();
+                        _selectedEvents.value = _getEventsForDay(_selectedDay!);
+                      },
+                      child: Text("Submit"),
+                    )
+                  ],
+                );
+              });
+        },
+        child: Icon(Icons.add),
+      ),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text("Selected Day =" + today.toString().split(" ")[0]),
+            Container(
+              child: TableCalendar(
+                //Pesonalizacion estetica
+                locale: "en_US", //idioma
+                rowHeight: 43,
+                headerStyle: HeaderStyle(
+                    formatButtonVisible: false, titleCentered: true),
+                availableGestures: AvailableGestures.all,
+                selectedDayPredicate: (day) => isSameDay(day, today),
+                focusedDay: today,
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                onDaySelected: _onDaySelected,
+                eventLoader: _getEventsForDay,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                  valueListenable: _selectedEvents,
+                  builder: (context, value, _) {
+                    return ListView.builder(
+                        itemCount: value.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 4),
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: ListTile(
+                              onTap: () => print(""),
+                              title: Text("${value[index].title}"),
+                            ),
+                          );
+                        });
+                  }),
+            )
+          ],
+        ));
   }
 }
