45efc14b976ced1344b9f9868f602f2d5ff76b2d
jmac-94 <josue.arriaga@utec.edu.pe>
2023-12-11 01:27:59 -0500

    refactor: Order Calendar and change language of Calendar to spanish

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'app/app.dart';
 import 'core/firebase_options.dart';
 
+import 'package:intl/date_symbol_data_local.dart';
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  initializeDateFormatting('es_MX', null);
 
   if (Platform.isIOS) {
     await Firebase.initializeApp(
